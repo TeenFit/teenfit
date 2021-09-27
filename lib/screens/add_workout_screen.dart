71ded6e8abe77121028d62104bef9a25511ad9d9
TeenFit <teenfitness.fit@gmail.com>
2021-09-27 18:48:45 -0400

    changers

--- lib/screens/add_workout_screen.dart
+++ lib/screens/add_workout_screen.dart
@@ -1,12 +1,41 @@
 import 'package:flutter/material.dart';
+import 'package:teenfit/providers/workout.dart';
 
 class AddWorkoutScreen extends StatelessWidget {
   static const routeName = '/add-workout-screen';
 
   @override
   Widget build(BuildContext context) {
+    final _mediaQuery = MediaQuery.of(context);
+    final _theme = Theme.of(context);
+    final _appBarHeight =
+        (AppBar().preferredSize.height + _mediaQuery.padding.top);
+
+    Workout? workout = ModalRoute.of(context)?.settings.arguments as Workout?;
+
     return Scaffold(
-      appBar: AppBar(),
+      backgroundColor: _theme.highlightColor,
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+        iconTheme: IconThemeData(color: Colors.white),
+        title: Text(
+          workout == null ? 'Add A Workout' : 'Edit Your Workout',
+          maxLines: 2,
+          textAlign: TextAlign.start,
+          style: TextStyle(
+            color: Colors.white,
+            fontFamily: 'Roboto',
+            fontSize: _mediaQuery.size.height * 0.03,
+            letterSpacing: 1,
+          ),
+        ),
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [],
+        ),
+      ),
     );
   }
 }
