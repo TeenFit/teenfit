a90808e07fe0fc66be63b06c5282d80c49480001
TeenFit <teenfitness.fit@gmail.com>
2021-10-04 15:44:18 -0400

    adding stufs

--- lib/providers/exercise.dart
+++ lib/providers/exercise.dart
@@ -2,12 +2,16 @@ class Exercise {
   final String name;
   final int timeSeconds;
   final int restTime;
+  final String? reps;
+  final String? sets;
   final String exerciseImageLink;
   final String exerciseId;
 
   Exercise({
     required this.exerciseId,
     required this.name,
+    this.reps,
+    this.sets,
     required this.timeSeconds,
     required this.restTime,
     required this.exerciseImageLink,
