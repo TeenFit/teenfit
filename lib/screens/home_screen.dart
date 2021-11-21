18e98b80c0cd5e742eae0896b0b5ceab373460d9
TeenFit <teenfitness.fit@gmail.com>
2021-11-21 15:36:06 -0500

    lol

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -92,7 +92,9 @@ class _HomeScreenState extends State<HomeScreen> {
     }
 
     Connectivity().onConnectivityChanged.listen((event) async {
-      await Phoenix.rebirth(context);
+      if (this.mounted) {
+        await Phoenix.rebirth(context);
+      }
     });
 
     super.didChangeDependencies();
