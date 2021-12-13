6c4e1600ddb2a1a59d47e8bac6e446389887a5b1
TeenFit <teenfitness.fit@gmail.com>
2021-12-12 22:21:14 -0500

    w

--- lib/screens/auth/loading.dart
+++ lib/screens/auth/loading.dart
@@ -11,11 +11,11 @@ class LoadingScreen extends StatelessWidget {
       resizeToAvoidBottomInset: false,
       body: Center(
         child: Container(
-          height: _mediaQuery.size.height * 0.5,
+          height: _mediaQuery.size.width * 0.5,
           width: _mediaQuery.size.width * 0.5,
           child: Image.asset(
             'assets/images/teen_fit_logo_white_withpeople 1@3x.png',
-            fit: BoxFit.cover,
+            fit: BoxFit.contain,
           ),
         ),
       ),
--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -155,11 +155,11 @@ class _HomeScreenState extends State<HomeScreen> {
       body: isLoading
           ? Center(
               child: Container(
-                height: _mediaQuery.size.height * 0.5,
+                height: _mediaQuery.size.width * 0.5,
                 width: _mediaQuery.size.width * 0.5,
                 child: Image.asset(
                   'assets/images/teen_fit_logo_white_withpeople 1@3x.png',
-                  fit: BoxFit.cover,
+                  fit: BoxFit.contain,
                 ),
               ),
             )
