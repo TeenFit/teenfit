82f7d9668fd3551d9163a8baf320d759d47ee63b
TeenFit <teenfitness.fit@gmail.com>
2021-12-16 12:20:13 -0500

    w

---
+++ lib/providers/person.dart
@@ -0,0 +1,11 @@
+class Person {
+  final String name;
+  final String email;
+  final List savedWorkouts;
+
+  Person({
+    required this.name,
+    required this.email,
+    required this.savedWorkouts,
+  });
+}
