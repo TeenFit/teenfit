58123791e0b006f86d1a5b270e36e576f3595417
TeenFit <teenfitness.fit@gmail.com>
2022-03-20 21:42:15 -0400

    w

--- lib/Custom/custom_dialog.dart
+++ lib/Custom/custom_dialog.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:fluttertoast/fluttertoast.dart';
-import 'package:teenfit/providers/auth.dart';
+// import 'package:teenfit/providers/auth.dart';
 import '../screens/exercise_screen.dart';
 import 'constants.dart';
 import 'package:provider/provider.dart';
