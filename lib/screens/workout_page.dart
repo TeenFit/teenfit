1e5c0e4a3283beae5c10a6adee47dedd3b776e60
TeenFitness <teenfitness.fit@gmail.com>
2022-04-05 17:32:46 -0400

    w

--- lib/screens/workout_page.dart
+++ lib/screens/workout_page.dart
@@ -132,155 +132,171 @@ class WorkoutPage extends StatelessWidget {
                           height: _mediaQuery.size.height * 0.35,
                           width: _mediaQuery.size.width * 0.9,
                           child: Column(
-                            mainAxisAlignment: MainAxisAlignment.start,
-                            crossAxisAlignment: CrossAxisAlignment.end,
                             children: [
-                              SizedBox(
-                                height: _statusBarHeight,
-                              ),
-                              Padding(
-                                padding: EdgeInsets.fromLTRB(
-                                  _mediaQuery.size.width * 0.02,
-                                  0,
-                                  _mediaQuery.size.width * 0.03,
-                                  _mediaQuery.size.width * 0.008,
-                                ),
-                                child: Text(
-                                  workout.workoutName,
-                                  maxLines: 2,
-                                  textAlign: TextAlign.end,
-                                  style: TextStyle(
-                                    color: Colors.white,
-                                    fontFamily: 'Roboto',
-                                    fontSize: _mediaQuery.size.height * 0.055,
-                                    letterSpacing: 1,
-                                    shadows: <Shadow>[
-                                      Shadow(
-                                        offset: Offset(2.5, 2.5),
-                                        blurRadius: 1.0,
-                                        color:
-                                            Color.fromARGB(255, 128, 128, 128),
+                              Container(
+                                height: _mediaQuery.size.height * 0.28,
+                                width: _mediaQuery.size.width * 0.9,
+                                child: Column(
+                                  mainAxisAlignment: MainAxisAlignment.start,
+                                  crossAxisAlignment: CrossAxisAlignment.end,
+                                  children: [
+                                    SizedBox(
+                                      height: _statusBarHeight,
+                                    ),
+                                    Padding(
+                                      padding: EdgeInsets.fromLTRB(
+                                        _mediaQuery.size.width * 0.02,
+                                        0,
+                                        _mediaQuery.size.width * 0.03,
+                                        _mediaQuery.size.width * 0.008,
                                       ),
-                                    ],
-                                  ),
+                                      child: Text(
+                                        workout.workoutName,
+                                        maxLines: 2,
+                                        textAlign: TextAlign.end,
+                                        style: TextStyle(
+                                          color: Colors.white,
+                                          fontFamily: 'Roboto',
+                                          fontSize:
+                                              _mediaQuery.size.height * 0.055,
+                                          letterSpacing: 1,
+                                          shadows: <Shadow>[
+                                            Shadow(
+                                              offset: Offset(2.5, 2.5),
+                                              blurRadius: 1.0,
+                                              color: Color.fromARGB(
+                                                  255, 128, 128, 128),
+                                            ),
+                                          ],
+                                        ),
+                                      ),
+                                    ),
+                                    Container(
+                                      alignment: Alignment.centerRight,
+                                      width: _mediaQuery.size.width,
+                                      child: Padding(
+                                        padding: EdgeInsets.fromLTRB(
+                                          _mediaQuery.size.width * 0.02,
+                                          0,
+                                          _mediaQuery.size.width * 0.03,
+                                          _mediaQuery.size.width * 0.008,
+                                        ),
+                                        child: Text(
+                                          'by: ${workout.creatorName}',
+                                          maxLines: 1,
+                                          style: TextStyle(
+                                            color: Colors.white,
+                                            fontFamily: 'PTSans',
+                                            fontSize:
+                                                _mediaQuery.size.height * 0.045,
+                                            letterSpacing: 1,
+                                            shadows: <Shadow>[
+                                              Shadow(
+                                                offset: Offset(2.5, 2.5),
+                                                blurRadius: 1.0,
+                                                color: Color.fromARGB(
+                                                    255, 128, 128, 128),
+                                              ),
+                                            ],
+                                          ),
+                                        ),
+                                      ),
+                                    ),
+                                  ],
                                 ),
                               ),
                               Container(
-                                alignment: Alignment.centerRight,
-                                width: _mediaQuery.size.width,
+                                height: _mediaQuery.size.height * 0.07,
+                                width: _mediaQuery.size.width * 0.9,
+                                alignment: Alignment.bottomRight,
                                 child: Padding(
-                                  padding: EdgeInsets.fromLTRB(
-                                    _mediaQuery.size.width * 0.02,
-                                    0,
-                                    _mediaQuery.size.width * 0.03,
-                                    _mediaQuery.size.width * 0.008,
-                                  ),
-                                  child: Text(
-                                    'by: ${workout.creatorName}',
-                                    maxLines: 1,
-                                    style: TextStyle(
-                                      color: Colors.white,
-                                      fontFamily: 'PTSans',
-                                      fontSize: _mediaQuery.size.height * 0.045,
-                                      letterSpacing: 1,
-                                      shadows: <Shadow>[
-                                        Shadow(
-                                          offset: Offset(2.5, 2.5),
-                                          blurRadius: 1.0,
-                                          color: Color.fromARGB(
-                                              255, 128, 128, 128),
-                                        ),
-                                      ],
-                                    ),
-                                  ),
-                                ),
-                              ),
-                              Padding(
-                                padding: EdgeInsets.symmetric(
-                                    horizontal: 15, vertical: 2),
-                                child: Container(
-                                  height: (_mediaQuery.size.height -
-                                          _appBarHeight) *
-                                      0.13,
-                                  width: _mediaQuery.size.width * 0.55,
-                                  child: FittedBox(
-                                    fit: BoxFit.fitHeight,
-                                    child: Row(
-                                      mainAxisAlignment:
-                                          MainAxisAlignment.start,
-                                      crossAxisAlignment:
-                                          CrossAxisAlignment.center,
-                                      children: [
-                                        workout.instagram.isEmpty
-                                            ? SizedBox()
-                                            : IconButton(
-                                                onPressed: () {
-                                                  try {
-                                                    launch(workout.instagram)
-                                                        .catchError((e) {
+                                  padding: EdgeInsets.symmetric(
+                                      horizontal: 15, vertical: 2),
+                                  child: Container(
+                                    height: (_mediaQuery.size.height -
+                                            _appBarHeight) *
+                                        0.13,
+                                    width: _mediaQuery.size.width * 0.55,
+                                    child: FittedBox(
+                                      fit: BoxFit.fitHeight,
+                                      child: Row(
+                                        mainAxisAlignment:
+                                            MainAxisAlignment.start,
+                                        crossAxisAlignment:
+                                            CrossAxisAlignment.center,
+                                        children: [
+                                          workout.instagram.isEmpty
+                                              ? SizedBox()
+                                              : IconButton(
+                                                  onPressed: () {
+                                                    try {
+                                                      launch(workout.instagram)
+                                                          .catchError((e) {
+                                                        _showToast(
+                                                            'Link Not Available');
+                                                      });
+                                                    } catch (e) {
                                                       _showToast(
                                                           'Link Not Available');
-                                                    });
-                                                  } catch (e) {
-                                                    _showToast(
-                                                        'Link Not Available');
-                                                  }
-                                                },
-                                                icon: Icon(
-                                                  MyFlutterApp.instagram_1,
-                                                  size:
-                                                      _mediaQuery.size.height *
-                                                          0.045,
-                                                  color: Colors.white,
+                                                    }
+                                                  },
+                                                  icon: Icon(
+                                                    MyFlutterApp.instagram_1,
+                                                    size: _mediaQuery
+                                                            .size.height *
+                                                        0.045,
+                                                    color: Colors.white,
+                                                  ),
                                                 ),
-                                              ),
-                                        workout.facebook.isEmpty
-                                            ? SizedBox()
-                                            : IconButton(
-                                                onPressed: () {
-                                                  try {
-                                                    launch(workout.facebook)
-                                                        .catchError((e) {
+                                          workout.facebook.isEmpty
+                                              ? SizedBox()
+                                              : IconButton(
+                                                  onPressed: () {
+                                                    try {
+                                                      launch(workout.facebook)
+                                                          .catchError((e) {
+                                                        _showToast(
+                                                            'Link Not Available');
+                                                      });
+                                                    } catch (e) {
                                                       _showToast(
                                                           'Link Not Available');
-                                                    });
-                                                  } catch (e) {
-                                                    _showToast(
-                                                        'Link Not Available');
-                                                  }
-                                                },
-                                                icon: Icon(
-                                                  MyFlutterApp.facebook_squared,
-                                                  size:
-                                                      _mediaQuery.size.height *
-                                                          0.045,
-                                                  color: Colors.white,
+                                                    }
+                                                  },
+                                                  icon: Icon(
+                                                    MyFlutterApp
+                                                        .facebook_squared,
+                                                    size: _mediaQuery
+                                                            .size.height *
+                                                        0.045,
+                                                    color: Colors.white,
+                                                  ),
                                                 ),
-                                              ),
-                                        workout.tiktokLink.isEmpty
-                                            ? SizedBox()
-                                            : IconButton(
-                                                onPressed: () {
-                                                  try {
-                                                    launch(workout.tiktokLink)
-                                                        .catchError((e) {
+                                          workout.tiktokLink.isEmpty
+                                              ? SizedBox()
+                                              : IconButton(
+                                                  onPressed: () {
+                                                    try {
+                                                      launch(workout.tiktokLink)
+                                                          .catchError((e) {
+                                                        _showToast(
+                                                            'Link Not Available');
+                                                      });
+                                                    } catch (e) {
                                                       _showToast(
                                                           'Link Not Available');
-                                                    });
-                                                  } catch (e) {
-                                                    _showToast(
-                                                        'Link Not Available');
-                                                  }
-                                                },
-                                                icon: Icon(
-                                                  MyFlutterApp.unknown,
-                                                  color: Colors.white,
-                                                  size:
-                                                      _mediaQuery.size.height *
-                                                          0.045,
+                                                    }
+                                                  },
+                                                  icon: Icon(
+                                                    MyFlutterApp.unknown,
+                                                    color: Colors.white,
+                                                    size: _mediaQuery
+                                                            .size.height *
+                                                        0.045,
+                                                  ),
                                                 ),
-                                              ),
-                                      ],
+                                        ],
+                                      ),
                                     ),
                                   ),
                                 ),
