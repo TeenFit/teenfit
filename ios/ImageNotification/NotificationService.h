6d6c44001431350ac0753585284353195158f88d
TeenFitness <teenfitness.fit@gmail.com>
2022-04-25 18:11:25 -0400

    w

---
+++ ios/ImageNotification/NotificationService.h
@@ -0,0 +1,12 @@
+//
+//  NotificationService.h
+//  ImageNotification
+//
+//  Created by Muqeeth Khan on 2022-04-25.
+//
+
+#import <UserNotifications/UserNotifications.h>
+
+@interface NotificationService : UNNotificationServiceExtension
+
+@end
