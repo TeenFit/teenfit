566dab1f91d8f523973b73eda7dda848f9ba1c3c
TeenFitness <teenfitness.fit@gmail.com>
2022-04-27 16:07:24 -0400

    w

--- lib/screens/view_follow.dart
+++ lib/screens/view_follow.dart
@@ -2,7 +2,6 @@ import 'package:cached_network_image/cached_network_image.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:flutterfire_ui/firestore.dart';
-import 'package:fluttertoast/fluttertoast.dart';
 import 'package:teenfit/providers/user.dart';
 import 'package:teenfit/screens/my_workouts.dart';
 
