6fefee389a49ef33f0e92d56e4d416d27f5e98a0
TeenFit <teenfitness.fit@gmail.com>
2022-06-02 23:47:47 -0400

    w

--- lib/providers/adState.dart
+++ lib/providers/adState.dart
@@ -6,10 +6,10 @@ class AdState with ChangeNotifier{
 
   String get interstitialAdUnit => Platform.isAndroid
       ? 'ca-app-pub-3940256099942544/8691691433'
-      : 'ca-app-pub-3940256099942544/5135589807';
+      : 'ca-app-pub-3605247207313682/6959471110';
 
    String get interstitialAdUnit2 => Platform.isAndroid
       ? 'ca-app-pub-3940256099942544/8691691433'
-      : 'ca-app-pub-3940256099942544/5135589807';
+      : 'ca-app-pub-3605247207313682/3164676581';
 
 }
